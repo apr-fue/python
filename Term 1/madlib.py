cf1c59606798239816a32ca271a4bda80457722a
April Fuentes <april.fuentes@tooeleschools.org>
2019-10-23 13:34:26 -0600

    10/19 madlibs game

---
+++ Term 1/madlib.py
@@ -0,0 +1,28 @@
+#april fuentes
+#madlibs
+#09/09/19
+username = input("What's your name?")
+username2 = input("What's your friends name?")
+word3 = input("Enter "+username2+"s pronoun. ex(his/her)")
+word1 = input("Enter a pronoun. ex(he/she)")
+word2 = input("Enter a different pronoun ex(him/her)")
+word4 = input("Enter a noun")
+word5 = input("Enter a noun")
+word6 = input("Enter a noun")
+word7 = input("Enter a noun")
+word8 = input("Enter a noun")
+word9 = input("Enter a place")
+word10 = input("Enter a noun")
+
+
+
+text = """
+"""+username+""" is a fool."""+username2+""" is an absolute buffoon. """+username+""" thinks """+username2+""" can challenge """+word2+""" in """+word2+""" own """+word9+"""?
+"""+username+""" thinks """+username2+""" can rebel against """+word2+""" """+word8+"""?
+"""+username+""" dare comes into """+word2+""" """+word7+""" and upturn """+word2+""" """+word4+""" and spill """+word5+""" in """+word2+""" """+word6+"""?
+"""+username2+""" thought """+username+""" was safe in """+word3+""" chain mail armor behind that screen of """+username+""".
+"""+word1+""" will take these """+word10+""" and destroy """+username2+""".
+"""+word1+""" didn’t want war. but """+word1+""" didn’t start it."""
+
+print(text)
+
